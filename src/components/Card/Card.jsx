e2b98ca55ef78ede7b333485064842e15dee295f
GuidoDealbera <guidojdealbera@gmail.com>
2023-02-26 17:43:44 -0300

    ESTILOS NB

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,16 +4,18 @@ export default function Card(props) {
       <div className={styles.contenedor}>
          <div className={styles.boton}>
          <button 
-         onClick={props.onClose}>X</button>
+         onClick={props.onClose}></button>
          </div>
-         
          <div>
          <img className={styles.image} src={props.image} alt={props.name} />
          </div>
          <div className={styles.divData}>
          <h2>{props.name}</h2>
+         <br/>
+         <div className={styles.cosita}>
          <h6>{props.species}</h6>
          <h6>{props.gender}</h6>
+         </div>
          </div>
       </div>
    );
